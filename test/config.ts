54618f8b686a5246ed2de7ffcbfc7e99fd2c02b3
Vijay Kumar <vijaya.guthi@modusbox.com>
2022-02-02 17:24:55 +0530

    Changed default values

--- test/config.ts
+++ test/config.ts
@@ -32,13 +32,13 @@
 import * as env from 'env-var';
 
 export const roleAssignmentSvcBasePath = env.get('ROLE_ASSIGNMENT_SVC_BASE_PATH')
-    .default('http://localhost:3008')
+    .default('http://role-assignment-service')
     .asUrlObject();
 
 // It's useful to parse this as a URL object for validation. But any query strings will be
 // discarded, only the origin and path will be used.
 const basePathUrl = env.get('ML_INGRESS_BASE_PATH')
-    .default('http://bofportal.test.infra.mojatest.live.internal')
+    .default('http://bofportal.yourdomain.com')
     .asUrlObject();
 export const mlIngressBasePath = `${basePathUrl.origin}${basePathUrl.pathname}`.replace(/\/*$/, '');
 export const proxyPrefix = 'proxy';
@@ -48,5 +48,5 @@ export const username = env.get('TEST_USER_NAME')
     .asString();
 
 export const password = env.get('TEST_USER_PASSWORD')
-    .default('wGdfOKOdi7TTc0UmTorpSLMYiOjgPZ')
+    .default('testpassword')
     .asString();
